3e444c993d436a5490eda95f870112960d0d688c
alberto15bma <83101168+alberto15bma@users.noreply.github.com>
2021-07-30 14:38:14 -0500

    cambios en frontend y backend

---
+++ Asp .Net Core/ApiSistema/ApiSistema/Controllers/InicioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiSistema.Sistema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSistema.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API EJECUTANDOSE CORRECTAMENTE.");
+        }
+        [HttpGet]
+        [Route("verificar")]
+        public IActionResult GetConexion()
+        {
+            return Ok(new Respuesta() { Estado = true, Mensaje = "ok" });
+        }
+    }
+}
